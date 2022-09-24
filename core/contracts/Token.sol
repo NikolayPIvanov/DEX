d50d19ab5e079ca40b0898fd367ea04230f8f4f1
Nikolay Ivanov <nick.ivanov98@gmail.com>
2022-09-23 23:00:16 -0700

    Adding more properties to erc-20 token

--- core/contracts/Token.sol
+++ core/contracts/Token.sol
@@ -1,13 +1,31 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
 import "hardhat/console.sol";
 
 // ERC-20 Token Standard: https://eips.ethereum.org/EIPS/eip-20
 contract Token {
-    string _name = "My Token";
+    string public name;
+    string public symbol;
+    uint256 public decimals;
+    uint256 public totalSupply;
 
-    function name() public view returns (string memory) {
-        return _name;
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _decimals,
+        uint256 _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply * (10**_decimals);
+
+        console.log(
+            "Deploying a Token with name %s symbol %s and %s decimals",
+            name,
+            symbol,
+            decimals
+        );
     }
 }
---
+++ core/test/Token.js
@@ -0,0 +1,55 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const tokens = (value, decimals = 'ether') => {
+    return ethers.utils.parseUnits(value, decimals);
+}
+
+const tokenParameters = {
+    name: "Token",
+    symbol: "NPI",
+    decimals: 18,
+    totalSupply: '1000000'
+}
+
+describe("Token Contract Tests", function () {
+    let token;
+
+    beforeEach(async () => {
+        token = await deployTokenContract(
+            tokenParameters.name,
+            tokenParameters.symbol,
+            tokenParameters.decimals,
+            tokenParameters.totalSupply);
+    });
+
+    describe('Deployment', () => {
+        it("Should deploy contract with correct name", async () => {
+            // Act & Assert
+            expect(await token.name()).to.equal(tokenParameters.name);
+        });
+
+        it("Should deploy contract with correct symbol", async () => {
+            // Act & Assert
+            expect(await token.symbol()).to.equal(tokenParameters.symbol);
+        });
+
+        it("Should deploy contract with correct decimals", async () => {
+            // Act & Assert
+            expect(await token.decimals()).to.equal(tokenParameters.decimals);
+        });
+
+        it("Should deploy contract with correct totalSupply", async () => {
+            // Act & Assert
+            expect(await token.totalSupply()).to.equal(tokens(tokenParameters.totalSupply));
+        });
+    });
+
+});
+
+async function deployTokenContract(name, symbol, decimals, totalSupply) {
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy(name, symbol, decimals, totalSupply);
+    await token.deployed();
+    return token;
+}
